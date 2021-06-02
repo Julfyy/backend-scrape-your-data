a6d8c6f28c1442b7bfbab9670d0f0a5a117c8925
Julfyy <bohdan7565@gmail.com>
2021-06-02 19:04:52 +0300

    test

--- Parser.js
+++ Parser.js
@@ -1,94 +1,98 @@
 const puppeteer = require('puppeteer');
 const Excel = require('exceljs');
 
-const { findMaxColumnSize } = require('./utils/arrayUtils')
+const { findMaxColumnSize } = require('./utils/arrayUtils');
 
 const getData = async (request) => {
-    const url = request.url;
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto(url);
+  const url = request.url;
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(url);
 
-    const data = await page.evaluate((selectors) => {
-        function stripHtml(html) {
-          let tmp = document.createElement('div');
-          tmp.innerHTML = html;
-          return tmp.textContent || tmp.innerText || "";
-        }
+  const data = await page.evaluate((selectors) => {
+    function stripHtml(html) {
+      let tmp = document.createElement('div');
+      tmp.innerHTML = html;
+      return tmp.textContent || tmp.innerText || '';
+    }
 
-        const result = [];
+    const result = [];
 
-        for (let selector of selectors) {
-            const elements = document.querySelectorAll(selector.tag);
-            let content = [];
+    for (let selector of selectors) {
+      const elements = document.querySelectorAll(selector.tag);
+      let content = [];
 
-            for (let i = 0; i < elements.length; i++) {
-                content.push(elements[i].innerHTML === undefined ? '' : stripHtml(elements[i].innerHTML).trim());
-            }
+      for (let i = 0; i < elements.length; i++) {
+        content.push(
+          elements[i].innerHTML === undefined
+            ? ''
+            : stripHtml(elements[i].innerHTML).trim()
+        );
+      }
 
-            result.push({ key: selector.title, content });
-        }
+      result.push({ key: selector.title, content });
+    }
 
-        return result;
-    }, request.selectors);
+    return result;
+  }, request.selectors);
 
-    browser.close();
-    return data;
+  browser.close();
+  return data;
 };
 
 const getXlsx = async (request) => {
-    const data = await getData(request);
+  const data = await getData(request);
 
-    let workbook = new Excel.Workbook();
+  let workbook = new Excel.Workbook();
 
-    workbook.creator = 'Me';
-    workbook.lastModifiedBy = 'Her';
-    workbook.created = new Date();
-    workbook.modified = new Date();
-    workbook.lastPrinted = new Date();
-    workbook.properties.date1904 = true;
+  workbook.creator = 'Me';
+  workbook.lastModifiedBy = 'Her';
+  workbook.created = new Date();
+  workbook.modified = new Date();
+  workbook.lastPrinted = new Date();
+  workbook.properties.date1904 = true;
 
-    workbook.views = [
-        {
-            x: 0,
-            y: 0,
-            width: 10000,
-            height: 20000,
-            firstSheet: 0,
-            activeTab: 1,
-            visibility: 'visible',
-        },
-    ];
-    let worksheet = workbook.addWorksheet('Sheet1');
+  workbook.views = [
+    {
+      x: 0,
+      y: 0,
+      width: 10000,
+      height: 20000,
+      firstSheet: 0,
+      activeTab: 1,
+      visibility: 'visible',
+    },
+  ];
+  let worksheet = workbook.addWorksheet('Sheet1');
 
-    let columns = [];
-    for (let i = 0; i < data.length; i++) {
-      columns.push({ header: data[i].key, key: `${i}`, width: 70});
+  let columns = [];
+  for (let i = 0; i < data.length; i++) {
+    columns.push({ header: data[i].key, key: `${i}`, width: 70 });
+  }
+  worksheet.columns = columns;
+
+  let formattedData = [];
+  const maxSize = findMaxColumnSize(data);
+  for (let i = 0; i < maxSize; i++) {
+    let row = [];
+    for (let elem of data) {
+      row.push(elem.content[i] === undefined ? '' : elem.content[i]);
     }
-    worksheet.columns = columns;
+    formattedData.push(row);
+  }
 
-    let formattedData = [];
-    const maxSize = findMaxColumnSize(data);
-    for (let i = 0; i < maxSize; i++) {
-      let row = [];
-      for (let elem of data) {
-        row.push(elem.content[i] === undefined ? '' : elem.content[i]);
-      }
-      formattedData.push(row);
-    }
-
-    for (let row of formattedData) {
-      let endRow = {};
-      let i = 0;
-      for (let item of row) {
-        const jsonItem = JSON.parse(`{ "${i++}": "${item}" }`)
-        Object.assign(endRow, jsonItem)
-      }
-      
-      worksheet.addRow(endRow);
+  for (let row of formattedData) {
+    let endRow = {};
+    let i = 0;
+    for (let item of row) {
+      const jsonItem = JSON.parse(`{ "${i++}": "${item}" }`);
+      Object.assign(endRow, jsonItem);
     }
 
-    return workbook;
+    worksheet.addRow(endRow);
+  }
+
+  return workbook;
 };
 
 const getTxt = async (request) => {
@@ -104,22 +108,21 @@ const getTxt = async (request) => {
     formattedData.push(row);
   }
 
-  let file = "";
+  let file = '';
   let i = 1;
   for (let row of formattedData) {
     let endRow = `${i}. `;
     for (let item of row) {
-      if (item != '')
-        endRow += `${item} |`
+      if (item != '') endRow += `${item} |`;
     }
-    i++
-    file += `\n${endRow}`
+    i++;
+    file += `\n${endRow}`;
   }
 
   return file;
-}
+};
 
 module.exports = {
   getXlsx,
-  getTxt
-}
+  getTxt,
+};
