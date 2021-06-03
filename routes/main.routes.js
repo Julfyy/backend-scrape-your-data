17967bf8b66855501a86a0e6f8496c56b2ff25a4
Julfyy <bohdan7565@gmail.com>
2021-06-03 09:03:08 +0300

    test

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -4,34 +4,36 @@ const router = Router();
 const { getXlsx, getTxt } = require('../Parser.js');
 
 router.post('/scrape/xlsx', (req, res) => {
-    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    res.setHeader("Content-Disposition", "attachment; filename=Data.xlsx");
+  res.setHeader(
+    'Content-Type',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+  );
+  res.setHeader('Content-Disposition', 'attachment; filename=Data.xlsx');
 
-    getXlsx(req.body)
-        .then(file => {
-            file.xlsx.write(res)
-                .then(function (_) {
-                    res.end();
-                });
-        })
-        .catch(e => {
-            console.error(e)
-            res.status(400).json({ status: 400, message: "Bad url." });
-        });
+  getXlsx(req.body)
+    .then((file) => {
+      file.xlsx.write(res).then(function (_) {
+        res.end();
+      });
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(400).json({ status: 400, message: 'Bad url.' });
+    });
 });
 
 router.post('/scrape/txt', (req, res) => {
-    res.setHeader('Content-Type', "application/octet-stream");
-    res.setHeader('Content-Disposition', 'attachment; filename=Data.txt');
+  res.setHeader('Content-Type', 'application/octet-stream');
+  res.setHeader('Content-Disposition', 'attachment; filename=Data.txt');
 
-    getTxt(req.body)
-        .then(file => {
-            res.send(file);
-        })
-        .catch(e => {
-            console.error(e)
-            res.status(400).json({ status: 400, message: "Bad url." });
-        });
+  getTxt(req.body)
+    .then((file) => {
+      res.send(file);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(400).json({ status: 400, message: 'Bad url' });
+    });
 });
 
 module.exports = router;
